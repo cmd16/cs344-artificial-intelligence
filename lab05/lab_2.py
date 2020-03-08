34b2c76826591c76cc2c3f64110d48141e5e1c9b
cmd16 <catherine.dejager10@gmail.com>
2020-03-08 00:41:19 -0500

    Finish lab

--- lab05/lab_2.py
+++ lab05/lab_2.py
@@ -13,9 +13,12 @@ print(enumeration_ask('Cancer', dict(Test1=T, Test2=T), cancer).show_approx())
 print(enumeration_ask('Cancer', dict(Test1=T, Test2=F), cancer).show_approx())
 
 '''
-These results make sense. One failed test has a pretty large impact on the probability of having cancer.
+These results make sense. Testing positive on both tests means it is more likely that you have cancer, but only 95% 
+because we still need to account for the rarity of cancer. One negative test result has a pretty large impact on the 
+probability of having cancer.
+
 P(Cancer|~Test2)=P(Cancer|~Test1) since both tests have the same probabilities.
 P(Cancer|~Test1)= a<P(Cancer)*P(~Test1|Cancer), P(~Cancer)*P(~Test1|~Cancer)>
 = a<0.01*0.1, 0.99*0.8>
-
+= <0.001261, 0.9987>
 '''
--- lab05/lab_3.py
+++ lab05/lab_3.py
@@ -9,8 +9,19 @@ happiness = BayesNet([
     ('Happy', 'Sunny Raise', {(T, T): 1.0, (T, F): 0.7, (F, T): 0.9, (F, F):0.1}),
     ])
 
+'''
+P(R|S) = P(R) because raise and sunny are independent. So P(R)=0.01
+'''
 print(enumeration_ask('Raise', dict(Sunny=T), happiness).show_approx())
+'''
+P(R|HS)= ??
+'''
 print(enumeration_ask('Raise', dict(Happy=T, Sunny=T), happiness).show_approx())
 
+'''
+These make sense to me: a raise is so unlikely that even being happy doesn't increase the probability of a raise by much.
+Because it is unlikely to be happy while it is not sunny and you have no raise, knowing that you are happy and it is not 
+sunny makes it more likely that you have a raise.
+'''
 print(enumeration_ask('Raise', dict(Happy=T), happiness).show_approx())
 print(enumeration_ask('Raise', dict(Happy=T, Sunny=F), happiness).show_approx())
