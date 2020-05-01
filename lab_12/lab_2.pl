4c012502be854c6afe258dee3f330615e29e5ba8
cmd16 <catherine.dejager10@gmail.com>
2020-05-01 17:41:06 +0100

    part of lab_2.pl

---
+++ lab_12/lab_2.pl
@@ -0,0 +1,21 @@
+/**
+Exercise 2.1, questions 1, 2, 8, 9, 14 - Give the necessary instantiations.
+
+1. bread  =  bread
+unifies
+
+2. ’Bread’  =  bread
+does not unify.
+
+8. food(X)  =  food(bread)
+unifies. necessary instantiation: food(X)  =  food(bread).
+
+9. food(bread,X)  =  food(Y,sausage)
+unifies. necessary instantiations: X = sausage, Y = bread.
+
+14. meal(food(bread),X)  =  meal(X,drink(beer))
+does not unify.
+
+Exercise 2.2 - Explain how Prolog does its unification and reasoning. If you have issues getting the results you’d expect, are there things you can do to game the system?
+*/
+
