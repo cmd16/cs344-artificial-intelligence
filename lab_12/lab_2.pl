0d919869912d074030033662eb58f5e663348285
cmd16 <catherine.dejager10@gmail.com>
2020-05-01 18:30:59 +0100

    more on lab_2.pl (TODO: better description)

--- lab_12/lab_2.pl
+++ lab_12/lab_2.pl
@@ -15,7 +15,51 @@ unifies. necessary instantiations: X = sausage, Y = bread.
 
 14. meal(food(bread),X)  =  meal(X,drink(beer))
 does not unify.
+*/
 
+/**
 Exercise 2.2 - Explain how Prolog does its unification and reasoning. If you have issues getting the results you’d expect, are there things you can do to game the system?
 */
 
+house_elf(dobby).
+witch(hermione).
+witch('McGonagall').
+witch(rita_skeeter).
+wizard(harry).
+magic(X):-  house_elf(X).
+magic(X):-  wizard(X).
+magic(X):-  witch(X).
+
+/**
+Which of the following queries are satisfied? Where relevant, give all the variable instantiations that lead to success.
+
+I got a lot of ERROR: Undefined procedure: wizard/1 (DWIM could not correct goal), so I added wizard(harry) so prolog knows what a wizard is.
+
+?-  magic(house_elf).
+Not satisfied (house_elf is a procedure not a variable). Before I corrected the Undefined procedure error, I was getting that error here.
+
+
+?-  wizard(harry).
+Before I corrected the Undefined procedure error, I was getting that error here.
+After adding it, wizard(harry) is true.
+
+?-  magic(wizard).
+Not satisfied (magic and wizard are both procedures).
+Before I corrected the Undefined procedure error, I was getting that error here.
+
+?-  magic('McGonagall').
+Satisfied.
+Before I corrected the Undefined procedure error, I was getting that error here.
+
+?-  magic(Hermione).
+Because Hermione starts with uppercase it is treated as a variable. Prolog goes through every possible assignment to Hermione.
+Hermione could be any of our things that are magic (dobby is the first item found).
+?- magic(Hermione).
+Hermione = dobby ;
+Hermione = harry ;
+Hermione = hermione ;
+Hermione = 'McGonagall' ;
+Hermione = rita_skeeter.
+
+Draw the search tree for the query magic(Hermione) .
+*/
